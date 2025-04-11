6bb915627ffdb9d89362ec6d3866909d1e044f12
ROSHNEEGOUDA <roshnee111103@gmail.com>
2025-04-12 01:22:49 +0530

    fix server

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: 'https://rise-up-01.vercel.app', // Allow frontend domain
-    // origin: 'http://localhost:5173', // Allow frontend domain
+    origin: 'https://rise-up-01.vercel.app',
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Allow cookies if needed
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
-app.options('*', cors());
+
+app.options('*', cors()); 
 
 
 app.use(cookieParser());
