4abd6dd50d05788502a5297789b7ea247bc8ab56
ROSHNEEGOUDA <roshnee111103@gmail.com>
2025-04-12 01:12:55 +0530

    reslove the deployment

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: 'https://rise-up-01.vercel.app', // Allow frontend domain
+    origin: 'https://rise-up-01.vercel.app/', // Allow frontend domain
     // origin: 'http://localhost:5173', // Allow frontend domain
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
