5642d44c9e8aba9016bdd3312c0bced82bd30f73
ROSHNEEGOUDA <roshnee111103@gmail.com>
2025-04-12 13:09:28 +0530

    fix the jwt error

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,84 +1,79 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'; 
 
 const userSchema = new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
-
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    age:{
-        type:Number,
-
+    age: {
+        type: Number,
     },
-    skills:{
-        type:[String],
-        default:[]
+    skills: {
+        type: [String],
+        default: []
     },
-    location:{
-        type:String,
-        default:""
+    location: {
+        type: String,
+        default: ""
     },
-    metadata:{
-        type:[Object],
-        default:[]
-    }   
-
+    metadata: {
+        type: [Object],
+        default: []
+    }
 },
-{
-    timestamps:true
-})
+    {
+        timestamps: true
+    });
 
-
-// hashing password using the pre hook middleware of mongoose 
-
+// Hashing password using the pre-save middleware
 userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) return next();
 
-    // further condition to check if password is modified or not
-    if (!this.isModified('password')) return next()
+    // Hash the password before saving
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
-
-// checking whether the password is correct or not
-
+// Password verification method
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
+    return await bcrypt.compare(password, this.password);
+};
 
+// Generate Access Token
 userSchema.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email,
-        username: this.username,
-        
+        username: this.username
     },
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        })
-}
+        });
+};
+
+// Generate Refresh Token
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign({
-        _id: this._id,
-       
+        _id: this._id
     },
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        })
-}
+        });
+};
 
-export const User = mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);
